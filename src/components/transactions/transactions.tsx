7ffc410fb29a9d7ea08e2e6945383d0ffba14f04
MForystek <micforystek@gmail.com>
2025-11-05 01:31:25 +0100

    Added inactive buttons for manipulating transactions

--- src/components/transactions/transactions.tsx
+++ src/components/transactions/transactions.tsx
@@ -1,17 +1,33 @@
 import useFetchTransactions from "../../hooks/fetchTransactions";
+import TransactionsActions from "./transactionsActions";
 import TransactionsTable from "./transactionsTable";
 
 export function IncomesPage() {
   const { loading, loadingMessage, transactions} = useFetchTransactions("INCOME");
-  return <TransactionsTable label={"Incomes"} loading={loading} loadingMessage={loadingMessage} transactions={transactions}/>;
+  return (
+    <>
+      <TransactionsActions />
+      <TransactionsTable label={"Incomes"} loading={loading} loadingMessage={loadingMessage} transactions={transactions}/>
+    </>   
+  );
 }
 
 export function ExpensesPage() {
   const { loading, loadingMessage, transactions } = useFetchTransactions("EXPENSE");
-  return <TransactionsTable label={"Expenses"} loading={loading} loadingMessage={loadingMessage} transactions={transactions}/>;
+  return (
+    <>
+      <TransactionsActions />
+      <TransactionsTable label={"Expenses"} loading={loading} loadingMessage={loadingMessage} transactions={transactions}/>
+    </>   
+  );
 }
 
 export function TransactionsPage() {
   const { loading, loadingMessage, transactions } = useFetchTransactions();
-  return <TransactionsTable label={"Transactions"} loading={loading} loadingMessage={loadingMessage} transactions={transactions}/>;
+  return (
+    <>
+      <TransactionsActions />
+      <TransactionsTable label={"Transactions"} loading={loading} loadingMessage={loadingMessage} transactions={transactions}/>
+    </>    
+  );
 }  
---
+++ src/components/transactions/transactionsActions.tsx
@@ -0,0 +1,9 @@
+export default function TransactionsActions() {
+  return (
+    <>
+      <button type="button" className="action-button">Add</button>
+      <button type="button" className="action-button">Edit</button>
+      <button type="button" className="action-button">Delete</button>
+    </>
+  );
+}
